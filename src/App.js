0d8c81b78449e91f345b550478e21ecff94a5756
Ankita <ankitakanakagiri@gmail.com>
2024-12-08 16:05:03 +0530

    init-database

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
+const User = require("./models/user")
 
 const app = express();
+app.use(express.json());
 
+app.post("/signup", async(req, res) => {
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("User signed up successfully");
+    }catch(e){
+        res.status(500).send("SignUp Error: " + e.message);
+    }
+})
+
+
+app.use("/", (err, req, res, next) => {
+    if(err){
+        res.status(500).send("Oops! something went wrong :(")
+    }
+})
 connectDB().then(() => {
     console.log("Database connected successfully...");
     app.listen(3000, ()=>{
@@ -10,10 +28,4 @@ connectDB().then(() => {
     })
 }).catch((err)=>{
     console.log("Database can't be connected");
-})
-
-app.use("/", (err, req, res, next) => {
-    if(err){
-        res.status(500).send("Oops! something went wrong :(")
-    }
-})
+});
